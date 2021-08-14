605292ad53e56dcc490b734db36d20600042c396
G. J. Gonzalez <88063887+gusgeet@users.noreply.github.com>
2021-08-14 20:46:23 -0300

    Ejemplos de JavaScript

---
+++ work.js
@@ -0,0 +1,39 @@
+let palabra="Muestra de texto aleatorio";
+let unNumero=15;
+let esVerda=true;
+let datosVarios=['Auto', 25, 'Vendido'];
+function nuevaFuncion(palabra){
+    console.log(palabra);
+}
+function nuevaFuncion2(unNumero){
+    console.log(unNumero);
+}
+function nuevaFuncion3(esVerda){
+    console.log(esVerda);
+}
+function nuevaFuncion4(datosVarios){
+    console.log(datosVarios);
+}
+nuevaFuncion(palabra);
+nuevaFuncion2(unNumero);
+nuevaFuncion3(esVerda);
+nuevaFuncion4(datosVarios);
+nuevaFuncion4(datosVarios[1]);
+
+
+let nombre = "Gustavo";
+let apellido = "Gonzalez";
+function identificar(){
+    console.log(nombre + " " + apellido);
+}
+identificar(nombre + " " + apellido);
+
+
+let animal = 'jirafa';
+let numero = animal.length;
+let resultado = Math.pow(numero, 3);
+resultado = resultado - 10.7;
+resultado = Math.round(resultado);
+console.log(resultado);
+
+
